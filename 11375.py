bbc8e4b7656f63a0d0add733960c790be51aa270
jintak0401 <jintak0401@naver.com>
2022-03-13 09:54:01 +0900

    이분매칭 [열혈강호]

--- 11375.py
+++ 11375.py
@@ -1,37 +1,42 @@
-from sys import stdin, setrecursionlimit as SRL
-SRL(10 ** 5)
+from sys import stdin
 
 input = stdin.readline
 
 
-# 이분매칭
-def bipartite_matching(vertex, here, connected_vertex, visited):
-    for there in vertex[here]:
-        if not visited[there]:
-            visited[there] = True
-            if (not connected_vertex[there]) or bipartite_matching(vertex, connected_vertex[there], connected_vertex, visited):
-                connected_vertex[there] = here
+def solve():
+    N, M = map(int, input().split())
+    # 직원에 해당하는 변수로 1인당 2개까지 일할 수 있으므로 1명을 2명으로 만들어준다
+    # left_vertex[i] = [i번째 직원이 할 수 있는 일들]
+    left_vertex = [0] * (N + 1)
+    for i in range(1, N + 1):
+        _, *works = map(int, input().split())
+        left_vertex[i] = works
+
+    # 일에 해당하는 변수
+    # right_vertex[i] = j --> i번째 일은 j번째 직원이 한다
+    right_vertex = [0] * (M + 1)
+
+    def bipartite_matching(visited, here):
+        visited[here] = True
+        for there in left_vertex[here]:
+            if not right_vertex[there]:
+                right_vertex[there] = here
                 return True
-
-    return False
-
-
-def solve(n, m):
-    worker = [0] * (n + 1)
-    for i in range(1, n + 1):
-        _, *worker[i] = [*map(int, input().split())]
+        for there in left_vertex[here]:
+            if not visited[right_vertex[there]] and bipartite_matching(visited, right_vertex[there]):
+                right_vertex[there] = here
+                return True
+        return False
 
     ans = 0
-    connected_vertex = [0] * (m + 1)
-
-    for i in range(1, len(worker)):
-        visited = [False] * (m + 1)
-        if bipartite_matching(worker, i, connected_vertex, visited):
+    for i in range(1, N+1):
+        if bipartite_matching([False]*(N+1), i):
             ans += 1
+            if ans == M:
+                break
 
     return ans
 
 
 if __name__ == '__main__':
-    N, M = map(int, input().split())
-    print(solve(N, M))
+    print(solve())
