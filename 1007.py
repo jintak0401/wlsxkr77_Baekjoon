216774a96de8a479263e5a347a37b4820ee5e49c
jintak0401 <jintak0401@naver.com>
2022-04-14 16:59:57 +0900

    Math, Brute Force [벡터 매칭]

--- 1007.py
+++ 1007.py
@@ -1,42 +1,37 @@
+"""
+Probliem Link: https://www.acmicpc.net/problem/1007
+Solution Link: https://jintak0401.github.io/posts/boj-1007
+"""
 from sys import stdin
-from itertools import combinations
 
 input = stdin.readline
 
 
 def solve():
     T = int(input())
-    inf = float('inf')
-    for _ in range(T):
-        N = int(input())
-        if N == 2:
-            pt1 = (*map(int, input().split()), 2)
-            pt2 = (*map(int, input().split()), 2)
-            print(((pt1[0] - pt2[0]) ** 2 + (pt1[1] - pt2[1]) ** 2) ** 0.5)
+    x, y = [0] * 20, [0] * 20
+    ans, tx, ty = 1e9, 0, 0
+
+    def combi(cnt, idx, sx, sy):
+        nonlocal ans
+        if cnt == 0:
+            tmp = ((tx - 2 * sx) ** 2 + (ty - 2 * sy) ** 2) ** 0.5
+            if tmp < ans:
+                ans = tmp
+
         else:
-            arr = [0] * N
-            arr[0] = (*map(int, input().split()),)
-            sum_x, sum_y = -arr[0][0], -arr[0][1]
-            for i in range(1, N):
-                arr[i] = (*map(int, input().split()),)
-                sum_x += arr[i][0]
-                sum_y += arr[i][1]
+            for i in range(idx, N - cnt + 1):
+                combi(cnt-1, i+1, sx+x[i], sy+y[i])
 
-            def get_sum(start_pt):
-                dx, dy = 0, 0
-                for pt in start_pt:
-                    dx += pt[0]
-                    dy += pt[1]
-                x, y = sum_x - 2*dx, sum_y - 2*dy
-                return (x ** 2 + y ** 2) ** 0.5
-
-            ans = inf
-
-            for start_pt in combinations([arr[i] for i in range(1, N)], N//2 - 1):
-                val = get_sum(start_pt)
-                if val < ans:
-                    ans = val
-            print(ans)
+    for t in range(T):
+        N = int(input())
+        ans, tx, ty = 1e9, 0, 0
+        for i in range(N):
+            x[i], y[i] = map(int, input().split())
+            tx += x[i]
+            ty += y[i]
+        combi(N//2 - 1, 1, x[0], y[0])
+        print(ans)
 
 
 if __name__ == '__main__':
