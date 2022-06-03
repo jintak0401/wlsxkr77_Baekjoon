8918bf8a82e67356842c64feb6a13fd40cdb1678
jintak0401 <jintak0401@naver.com>
2022-06-03 12:03:24 +0900

    이분탐색, MITM [냅색문제]

---
+++ 1450.py
@@ -0,0 +1,39 @@
+from sys import stdin
+
+input = stdin.readline
+
+
+def solve():
+    N, C = map(int, input().split())
+    arr = [*map(int, input().split())]
+
+    w1, w2 = [0], [0]
+
+    for w in arr[:N // 2]:
+        tmp = []
+        for x in w1:
+            if (val := x + w) <= C:
+                tmp.append(val)
+        w1 += tmp
+
+    for w in arr[N // 2:]:
+        tmp = []
+        for x in w2:
+            if (val := x + w) <= C:
+                tmp.append(val)
+        w2 += tmp
+
+    w1.sort()
+    w2.sort()
+
+    j = len(w2) - 1
+    ans = 0
+    for i in range(len(w1)):
+        if j < 0: break
+        while w1[i] + w2[j] > C: j -= 1
+        ans += j + 1
+    return ans
+
+
+if __name__ == '__main__':
+    print(solve())
