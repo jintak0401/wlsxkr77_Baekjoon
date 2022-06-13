2af3a3be480c4851e55a4d795589b353d0e22abe
jintak0401 <jintak0401@naver.com>
2022-06-13 09:34:08 +0900

    수학 [너 봄에는 캡사이신이 맛있단다]

--- 15824.py
+++ 15824.py
@@ -3,17 +3,15 @@ from sys import stdin
 input = stdin.readline
 
 
-def solve(N, arr):
+def solve():
     mod = 1_000_000_007
+    N = int(input())
+    arr = [*map(int, input().split())]
     if N == 1: return 0
     arr.sort()
     power = [1] * N
-    power[0] = 0
-    power[1] = 2
-    for i in range(2, N):
+    for i in range(1, N):
         power[i] = (2 * power[i-1]) % mod
-        power[i-1] -= 1
-    power[-1] -= 1
 
     ans = 0
     for i in range(N // 2):
