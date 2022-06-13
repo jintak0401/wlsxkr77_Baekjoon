2c22636125f6751f4039fd74d208e95331207205
jintak0401 <jintak0401@naver.com>
2022-06-13 09:06:25 +0900

    [너 봄에는 캡사이신이 맛있단다]

---
+++ 15824.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+
+function solve() {
+	// BigInt 사용
+	const MOD = 1_000_000_007n;
+	const input = fs.readFileSync('/dev/stdin').toString().split('\n');
+	let inputIdx = 0;
+	const N = +input[inputIdx++];
+	const arr = input[inputIdx++]
+		.split(' ')
+		.map((v) => BigInt(v))
+		.sort((a, b) => Number(a - b));
+
+	if (N === 1) return 0;
+
+	const power = [];
+	power.length = N;
+	power[0] = 0n;
+	power[1] = 2n;
+	for (let i = 2; i < N; i++) {
+		power[i] = (2n * power[i - 1]) % MOD;
+		power[i - 1] -= 1n;
+	}
+	power[N - 1] -= 1n;
+
+	let tmp,
+		ans = 0n;
+	for (let i = 0; i < Math.floor(N / 2); i++) {
+		tmp = N - 1 - i;
+		ans = (ans + (arr[tmp] - arr[i]) * (power[tmp] - power[i])) % MOD;
+		// JS 는 -5 % 3 === -2 이므로 음수일 경우 +MOD 해줌
+		if (ans < 0) ans += MOD;
+	}
+
+	return Number(ans);
+}
+
+console.log(solve());
