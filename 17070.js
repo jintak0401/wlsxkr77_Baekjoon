d2da96bc87c7ab8171131386a16f8e4424418e41
jintak0401 <jintak0401@naver.com>
2022-12-12 23:30:24 +0900

    DP [파이프 옮기기 1]

---
+++ 17070.js
@@ -0,0 +1,51 @@
+const fs = require('fs');
+
+function solve() {
+    let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+    let inputIdx = 0;
+
+    const N = +input[inputIdx++];
+    const map = [];
+    map.length = N;
+    for (let i = 0; i < N; i++){
+        map[i] = input[inputIdx++].split(' ');
+    }
+
+    // 0: 가로, 1: 대각, 2: 세로
+    const dp = [];
+    dp.length = 3;
+    for (let i = 0; i < 3; i++){
+        dp[i] = [];
+        dp[i].length = N;
+        for (let j = 0; j < N; j++) {
+            dp[i][j] = [];
+            dp[i][j].length = N;
+            for (let k = 0; k < N; dp[i][j][k++] = 0);
+        }
+    }
+    dp[0][0][1] = 1;
+
+    for (let j = 0; j < N; j++){
+        for (let k = 2; k < N; k++){
+            for (let i = 0; i < 3; i++){
+                switch (i) {
+                    case 0:
+                        if (map[j][k] === '1') continue;
+                        dp[0][j][k] += dp[0][j][k-1] + dp[1][j][k-1];
+                        break
+                    case 1:
+                        if (map[j][k] === '1' || map[j][k-1] === '1' || (0 < j && map[j-1][k] === '1')) continue;
+                        dp[1][j][k] += j >= 1 ? (dp[0][j-1][k-1] + dp[1][j-1][k-1] + dp[2][j-1][k-1]) : 0;
+                        break;
+                    case 2:
+                        if (map[j][k] === '1') continue;
+                        dp[2][j][k] += j >= 1 ? (dp[1][j-1][k] + dp[2][j-1][k]) : 0;
+                        break;
+                }
+            }
+        }
+    }
+    console.log(dp[0][N-1][N-1] + dp[1][N-1][N-1] + dp[2][N-1][N-1]);
+}
+
+solve();
